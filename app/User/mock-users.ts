d4a40cfe593b946c1ddf8616b18f7fa2faa34664
uraway <masato.u0107@gmail.com>
2016-11-17 18:41:54 +0900

    add ranking

---
+++ app/User/mock-users.ts
@@ -0,0 +1,14 @@
+import { User } from './User';
+
+export const USERS: User[] = [
+  { id: 1, name: 'Mr. Nice', score: 30 },
+  { id: 2, name: 'Narco', score: 78 },
+  { id: 3, name: 'Bombasto', score: 65 },
+  { id: 4, name: 'Celeritas', score: 32 },
+  { id: 5, name: 'Magneta', score: 66 },
+  { id: 6, name: 'RubberMan', score: 87 },
+  { id: 7, name: 'Dynama', score: 18 },
+  { id: 8, name: 'Dr IQ', score: 0 },
+  { id: 9, name: 'Magma', score: 0 },
+  { id: 10, name: 'Tornado', score: 2 }
+];
---
+++ app/User/user.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { User } from './user';
+import { USERS } from './mock-users';
+
+@Injectable()
+export class UserService {
+  getUsers(): Promise<User[]> {
+    return Promise.resolve(USERS);
+  }
+}
