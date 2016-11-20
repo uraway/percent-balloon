90614600c2a0a7b089a5fae5db9b7ee6f1a640d1
uraway <masato.u0107@gmail.com>
2016-11-20 12:34:26 +0900

    use sort by score method

--- app/Dashboard/dashboard.component.ts
+++ app/Dashboard/dashboard.component.ts
@@ -7,55 +7,7 @@ import { UserService } from '../User/user.service';
   moduleId: module.id,
   selector: 'my-dashboard',
   templateUrl: 'dashboard.component.html',
-  styles: [`
-    .selected {
-      background-color: #CFD8DC !important;
-      color: white;
-    }
-    .users {
-      margin: 0 0 2em 0;
-      list-style-type: none;
-      padding: 0;
-      width: 15em;
-    }
-    .users li {
-      cursor: pointer;
-      position: relative;
-      left: 0;
-      background-color: #EEE;
-      margin: .5em;
-      padding: .3em 0;
-      height: 1.6em;
-      border-radius: 4px;
-    }
-    .users li.selected:hover {
-      background-color: #BBD8DC !important;
-      color: white;
-    }
-    .users li:hover {
-      color: #607D8B;
-      background-color: #DDD;
-      left: .1em;
-    }
-    .users .text {
-      position: relative;
-      top: -3px;
-    }
-    .users .badge {
-      display: inline-block;
-      font-size: small;
-      color: white;
-      padding: 0.8em 0.7em 0 0.7em;
-      background-color: #607D8B;
-      line-height: 1em;
-      position: relative;
-      left: -1px;
-      top: -4px;
-      height: 1.8em;
-      margin-right: .8em;
-      border-radius: 4px 0 0 4px;
-    }
-  `],
+  styleUrls: ['dashboard.componet.css'],
   providers: [UserService]
 })
 export class DashboardComponent implements OnInit {
@@ -64,7 +16,16 @@ export class DashboardComponent implements OnInit {
   constructor(private userService: UserService) { }
 
   getUsers(): void {
-    this.userService.getUsers().then(users => this.users = users);
+    this.userService.getUsers().then(users => this.users = this.sortByScore(users));
+  }
+
+  sortByScore(users: User[]): User[] {
+    users.sort((a: any, b: any) => {
+      if (a.score > b.score) return -1;
+      if (b.score > a.score) return 1;
+      return 0;
+    })
+    return users;
   }
 
   ngOnInit(): void {
