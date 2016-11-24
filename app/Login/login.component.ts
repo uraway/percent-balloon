e37ff5b3e65b5e57a2f1f5bcf978cb752cf97c7c
uraway <masato.u0107@gmail.com>
2016-11-24 17:57:44 +0900

    userName login

--- app/Login/login.component.ts
+++ app/Login/login.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   moduleId: module.id,
   selector: 'login',
   templateUrl: 'login.component.html',
 })
-export class LoginComponent { }
+export class LoginComponent {
+
+  constructor(private router: Router) { }
+
+  login(event: any, userName: string) {
+    event.preventDefault();
+    localStorage.setItem('userName', userName);
+    this.router.navigate(['dashboard']);
+    console.log('login as ' + userName);
+  }
+}
+
